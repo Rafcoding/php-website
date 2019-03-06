c47f4a74c0b8b3836548fa7a84b5c3355b008536
Rafa <rafaelruizbernal@gmail.com>
2019-03-06 19:48:48 +0000

    homepage and css

--- website/index.php
+++ website/index.php
@@ -1,14 +1,59 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-<?php
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8">
+  <title>Music All Day</title>
+</head>
 
-echo "Hello World!"
+<body id="website">
 
-?>
-  </body>
+  <div class="container">
+
+    <div class="banner">
+      <a href="/" title="Home">
+        <img src="images/chris-tom-final.jpg" alt="banner-img">
+      </a>
+    </div><!-- banner -->
+
+    <div class="navbar">
+
+    </div><!-- navbar -->
+
+    <div class="content">
+      <div id="footer" class="cf">
+
+        <div class="column three">
+          <strong>Phone</strong>
+          11111111111
+        </div><!-- columns -->
+
+        <div class="column three">
+          <strong>Adress</strong>
+          1 Baltimore Whar, E14, London
+        </div><!-- columns -->
+
+        <div class="column three last">
+          <strong>Office Time</strong>
+          <em>Monday-Friday</em><br>
+          9:00am - 5:00pm<br><br>
+          <em>Saturday-Friday</em><br>
+          9:00am - 3:00pm<br><br>
+          <em>Sunday</em><br>
+          Closed<br><br>
+
+        </div><!-- columns -->
+
+      </div><!-- footer -->
+
+      <small>&copy;2019 Powered by Rafa Ruiz</small>
+
+    </div><!-- content -->
+
+
+  </div><!-- container -->
+
+
+
+</body>
 </html>
